20344878c655b6ba2f2bc3140ffc4080fc4dd206
Danny Pifer <piferdg@gmail.com>
2018-12-11 15:09:41 -0700

    Adding better solution that removes the other letters when clicking on different show elements

--- app.js
+++ app.js
@@ -1,74 +1,102 @@
-let showA = document.getElementsByClassName('show-A')[0]
-let showB = document.getElementsByClassName('show-B')[0]
-let showC = document.getElementsByClassName('show-C')[0]
-let resetButton = document.getElementsByClassName('reset-button')[0]
+let showA = document.getElementsByClassName("show-A")[0];
+let showB = document.getElementsByClassName("show-B")[0];
+let showC = document.getElementsByClassName("show-C")[0];
+let resetButton = document.getElementsByClassName("reset-button")[0];
 
-let showAButton = document.createElement('button')
-let showBButton = document.createElement('button')
-let showCButton = document.createElement('button')
-let reset = document.createElement('button')
+let showAButton = document.createElement("button");
+let showBButton = document.createElement("button");
+let showCButton = document.createElement("button");
+let reset = document.createElement("button");
 
-showAButton.textContent = "Show Letter A"
-showBButton.textContent = "Show Letter B"
-showCButton.textContent = "Show Letter C"
-reset.textContent = "Reset"
+let letterA = document.getElementsByClassName("letter-a")[0];
+let letterB = document.getElementsByClassName("letter-b")[0];
+let letterC = document.getElementsByClassName("letter-c")[0];
 
-showA.appendChild(showAButton)
-showAButton.addEventListener('click', showLetterA)
+showAButton.textContent = "Show Letter A";
+showBButton.textContent = "Show Letter B";
+showCButton.textContent = "Show Letter C";
+reset.textContent = "Reset";
 
-showB.appendChild(showBButton)
-showBButton.addEventListener('click', showLetterB)
+showA.appendChild(showAButton);
+showAButton.addEventListener("click", showLetterA);
 
-showC.appendChild(showCButton)
-showCButton.addEventListener('click', showLetterC)
+showB.appendChild(showBButton);
+showBButton.addEventListener("click", showLetterB);
 
-resetButton.appendChild(reset)
-reset.addEventListener('click', resetLetters)
+showC.appendChild(showCButton);
+showCButton.addEventListener("click", showLetterC);
+
+resetButton.appendChild(reset);
+reset.addEventListener("click", resetLetters);
 
 function showLetterA() {
-  let letterA = document.getElementsByClassName('letter-a')[0];
   if (letterA.style.display === "block") {
     letterA.style.display = "none";
   } else {
     letterA.style.display = "block";
   }
+  if (letterB.style.display === "block") {
+    letterB.style.display = "none";
+  } else {
+    letterB.style.display = "none";
+  }
+  if (letterC.style.display === "block") {
+    letterC.style.display = "none";
+  } else {
+    letterC.style.display = "none";
+  }
 }
 
 function showLetterB() {
-  let letterB = document.getElementsByClassName('letter-b')[0];
   if (letterB.style.display === "block") {
     letterB.style.display = "none";
   } else {
     letterB.style.display = "block";
   }
+  if (letterA.style.display === "block") {
+    letterA.style.display = "none";
+  } else {
+    letterA.style.display = "none";
+  }
+  if (letterC.style.display === "block") {
+    letterC.style.display = "none";
+  } else {
+    letterC.style.display = "none";
+  }
 }
 
 function showLetterC() {
-  let letterC = document.getElementsByClassName('letter-c')[0];
   if (letterC.style.display === "block") {
     letterC.style.display = "none";
   } else {
     letterC.style.display = "block";
   }
+  if (letterA.style.display === "block") {
+    letterA.style.display = "none";
+  } else {
+    letterA.style.display = "none";
+  }
+  if (letterB.style.display === "block") {
+    letterB.style.display = "none";
+  } else {
+    letterB.style.display = "none";
+  }
 }
 
 function resetLetters() {
-  let letterA = document.getElementsByClassName('letter-a')[0];
-  let letterB = document.getElementsByClassName('letter-b')[0];
-  let letterC = document.getElementsByClassName('letter-c')[0];
   if (letterA.style.display === "block") {
-        letterA.style.display = "none";
-      } else {
-        letterA.style.display = "none";
-      }
-      if (letterB.style.display === "block") {
-            letterB.style.display = "none";
-          } else {
-            letterB.style.display = "none";
-          }
-      if (letterC.style.display === "block") {
-        letterC.style.display = "none";
-      } else {
-        letterC.style.display = "none";
-      }
+    letterA.style.display = "none";
+  } else {
+    letterA.style.display = "none";
+  }
+  if (letterB.style.display === "block") {
+    letterB.style.display = "none";
+  } else {
+    letterB.style.display = "none";
+  }
+  if (letterC.style.display === "block") {
+    letterC.style.display = "none";
+  } else {
+    letterC.style.display = "none";
+  }
 }
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Code Challenge 
+### Daniel Pifer
+
+## Purpose 
+ - The purpose of this challenge was to click the 'Show' elements and have the corresponding A, B, or C elements appear on the page and hide the others. 
+
+## Deployed link
+ - xyz
